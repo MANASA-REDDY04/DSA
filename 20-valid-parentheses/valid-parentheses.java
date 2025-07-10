f61f191e34b593367ee86498e0fb1b18cd267924
Manasa Reddy <kandadimanasareddy12@gmail.com>
2025-07-10 10:09:28 +0530

    Time: 2 ms (96.78%) | Memory: 41.8 MB (47.78%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        int n = s.length();
+        for(int i = 0; i < n; i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '[' || ch == '{') st.push(ch);
+            else if(!st.isEmpty()){
+                char top = st.pop();
+                if(ch == ')' && top != '(' ||
+                    ch == '}' && top != '{' ||
+                    ch == ']' && top != '[') return false;
+            }
+            else return false;
+        }
+        return st.isEmpty();
+    }
+}
