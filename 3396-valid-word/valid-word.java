6fe101d2ddbaed40365843a7b86694d6a4005881
Manasa Reddy <kandadimanasareddy12@gmail.com>
2025-07-15 09:21:02 +0530

    Time: 1 ms (99.12%) | Memory: 42.4 MB (19.87%) - LeetSync

---
+++ 3396-valid-word/valid-word.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String word) {
+        int n = word.length();
+        if (n < 3)
+            return false;
+        boolean vowel = false;
+        boolean cons = false;
+        word = word.toLowerCase();
+        for (int i = 0; i < n; i++) {
+            char ch = word.charAt(i);
+            if (!(Character.isLetter(ch) || Character.isDigit(ch))) return false;
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowel = true;
+                } else
+                    cons = true;
+            }
+        }
+        return vowel && cons;
+    }
+}
