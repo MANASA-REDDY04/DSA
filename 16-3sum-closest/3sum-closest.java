2a1b919c8acb7c059ace3cc5d38a2cd36d533777
Manasa Reddy <kandadimanasareddy12@gmail.com>
2026-02-06 21:08:08 +0530

    Time: 18 ms (37.75%) | Memory: 45.7 MB (25.84%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -4,7 +4,7 @@ class Solution {
         int ans = 0;
         int n = nums.length;
         Arrays.sort(nums);
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n-2; i++){
             if(i > 0 && nums[i] == nums[i-1]) continue;
             int j = i+1;
             int k = n-1;
