32cdc7547e0957e6bd5dbc561fe5f5e36ad1ff5e
Manasa Reddy <kandadimanasareddy12@gmail.com>
2026-09-18 11:35:07 +0530

    Time: 2530 ms (5.00%) | Memory: 55.7 MB (97.14%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,32 +1,20 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        HashSet<List<Integer>> hs = new HashSet<>();
+        Set<List<Integer>> hs = new HashSet<>();
         int n = nums.length;
-        Arrays.sort(nums);
-        for(int i = 0; i < n; i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-            int j = i+1;
-            int k = n-1;
-            while(j < k){
-                int sum = nums[i]+nums[j]+nums[k];
-                if(sum == 0){
-                    List<Integer> li = new ArrayList<>();
-                    li.add(nums[i]);
-                    li.add(nums[j]);
-                    li.add(nums[k]);
-                    Collections.sort(li);
-                    hs.add(li);
-                    j++;
-                    k--;
-                    while(j < k && nums[j] == nums[j-1]) j++;
-                    while(j < k && nums[k] == nums[k+1]) k--;
+        for(int i = 0; i <= n-2; i++){
+            HashSet<Integer> hs2 = new HashSet<>();
+            for(int j = i+1; j < n; j++){
+                int third = -(nums[i]+nums[j]);
+                if(hs2.contains(third)){
+                    List<Integer> ls = Arrays.asList(nums[i],nums[j], third);
+                    ls.sort(null);
+                    hs.add(ls);
                 }
-                else if(sum < 0) j++;
-                else k--;
+                 hs2.add(nums[j]);
             }
         }
-        for(List<Integer> li: hs) ans.add(li);
-        return ans;
+        List<List<Integer>> res = new ArrayList<>(hs);
+        return res;
     }
 }
